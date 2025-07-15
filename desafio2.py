139be74b9537638e999ed37f63498ddb2f337f96
Lucas <alcantaralucas2002@gmail.com>
2025-07-15 00:22:03 -0300

    Revisão no Código adicionando comentários

--- desafio2.py
+++ desafio2.py
@@ -1,4 +1,6 @@
-# Pede idade e nome e exibe-os
+# Revisão no Código: 15/07/2025
+# Projeto: Inserir (Pelo Input) informações em String e Inteiro (com método Strip) 
+# e exibe os valores formatado em F-String.
 idade = int(input("\nQuantos anos você tem? ").strip())
 nome = str(input("\nQual seu nome ? ").strip())
 
