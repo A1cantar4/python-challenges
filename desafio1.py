24d18b0d9d99694c21d31868c962898b0398f9c8
Lucas <alcantaralucas2002@gmail.com>
2025-07-15 00:11:17 -0300

    Revisão no Código para exibir valor da variável e o tipo

--- desafio1.py
+++ desafio1.py
@@ -1,2 +1,5 @@
-# Exibe um texto
-print("Olá, estou aprendendo Python")
+# Revisão no Código: 15/07/2025
+# Projeto: Programa para exibir: Valor de Variável e Seu Tipo.
+msg = "Olá, estou aprendendo Python"
+print(msg)
+print(type(msg))
