ffebaaf88d9f64f466ab2dbc9e9d6720c5dbb536
Lucas <alcantaralucas2002@gmail.com>
2025-09-26 21:03:53 -0300

    Adicionado colorama e reorganizado string final para facilitar manutençao.

--- desafio2.py
+++ desafio2.py
@@ -1,7 +1,14 @@
-# Revisão no Código: 15/07/2025
+# Revisão no Código: 26/09/2025
 # Projeto: Inserir (Pelo Input) informações em String e Inteiro (com método Strip) 
 # e exibe os valores formatado em F-String.
-idade = int(input("\nQuantos anos você tem? ").strip())
-nome = str(input("\nQual seu nome ? ").strip())
+from assets.static.styles import c_verde, c_ciano, c_magenta
 
-print(f"\n Você se chama {nome}, e tem {idade} anos!")
+idade = int(input(c_verde("\nQuantos anos você tem? ")).strip())
+nome = str(input(c_ciano("\nQual seu nome ? ")).strip().title())
+
+# Adicionado identação para facilitar manutenção das cores
+print(str(c_magenta("\n Você se chama " + 
+                    c_verde(f"{nome}") + 
+                    c_magenta(", e tem ") + 
+                    c_ciano(f"{idade}") +
+                    c_magenta(" anos!"))))
