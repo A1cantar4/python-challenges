418ad739d29a32b9f4f240c4c43657d1439b5b37
Lucas <alcantaralucas2002@gmail.com>
2025-09-26 21:03:05 -0300

    Adicionado colorama para personalizar strings e modificado exibiçao do tipo da variável

--- desafio1.py
+++ desafio1.py
@@ -1,5 +1,19 @@
-# Revisão no Código: 15/07/2025
+# Revisão no Código: 26/09/2025
 # Projeto: Programa para exibir: Valor de Variável e Seu Tipo.
-msg = "Olá, estou aprendendo Python"
+# [Adicionado verificador para exibir o tipo de forma personalizada].
+from assets.static.styles import c_magenta, c_ciano, c_verde, c_amarelo
+
+msg = 'Sou um texto!'
 print(msg)
-print(type(msg))
+print(c_magenta(type(msg)))
+
+if type(msg) == str:
+    print(c_ciano('Olá, sou uma String!'))
+elif type(msg) == int:
+    print(c_verde('Olá, sou um número Inteiro!'))
+elif type(msg) == float:
+    print(c_verde('Olá, sou um número "Flutuante"!'))
+elif type(msg) == bool:
+    print(c_verde('Olá, posso ser Verdadeiro ou Falso!'))
+else:
+    print(c_amarelo('Olá, pena que não sou uma String!'))
